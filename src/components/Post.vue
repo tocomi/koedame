a65291a36fe1bc98f50483949f1631ab266c433b
tocomi <tocomi@wish.ocn.ne.jp>
2019-04-30 00:27:39 +0900

    set textbox

---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div#post
+    textarea.content
+    v-btn#submit(color="info")
+      v-icon(color="white") create
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from "vue-property-decorator";
+  
+  @Component
+  export default class Tweet extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+.content {
+  height: 70px;
+  width: 250px;
+  padding: 5px;
+  background-color: #FFF;
+  border: 1px solid #EAEAEA;
+}
+#submit {
+  margin: -30px 0 0 10px;
+}
+</style>
