4978d72827f0e7a2eea74d4742bd9307eee12e29
tocomi <tocomi@wish.ocn.ne.jp>
2019-04-29 19:13:33 +0900

    modify header

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,15 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>koedame</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://twitter.com/tocomi0112"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">@tocomi</span>
       </v-btn>
     </v-toolbar>
 
