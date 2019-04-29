cc10ad4014bc6e0b60bc5a9ce9e429efb4956593
tocomi <tocomi@wish.ocn.ne.jp>
2019-04-29 21:06:13 +0900

    change tweet layout

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-  v-card.tweet-card
-    v-avatar.tweet-avatar
+  v-card.card(flat)
+    v-avatar.avatar
       v-img(:src="`http://domonet.jp/plus/images/post/201805/thum-20180529093519.jpg`" alt="avatar")
-    p {{ tweet.user }}
-    p {{ tweet.content }}
+    p.user {{ tweet.user }}
+    p.content {{ tweet.content }}
 </template>
 
 <script lang="ts">
@@ -18,4 +18,22 @@
 </script>
 
 <style lang="scss" scoped>
+.card {
+  text-align: left;
+  height: 80px;
+  border-bottom: 1px solid #EEEEEE;
+  .avatar {
+    position: absolute;
+    margin: 10px 0 0 10px;
+  }
+  .user {
+    position: absolute;
+    margin: 10px 0 0 70px;
+    font-weight: bold;
+  }
+  .content {
+    position: absolute;
+    margin: 30px 0 0 70px;
+  }
+}
 </style>
