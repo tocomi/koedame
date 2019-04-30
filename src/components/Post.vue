778ddc325a150cf7bb734f29770221bf0906f3a3
tocomi <tocomi@wish.ocn.ne.jp>
2019-05-01 00:05:30 +0900

    add user area

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   div#post
+    div#user-area
+      input#user(v-model="user" placeholder="お名前")
     div#content-area
-      textarea#content(v-model="content" placeholder="思いの丈を、ぶちまけろ")
+      textarea#content(v-model="content" placeholder="なんでもどうぞ")
       v-btn#submit(color="info" @click="postTweet()" fab small)
         v-icon(color="white") create
 </template>
@@ -12,29 +14,46 @@
   @Component
   export default class Post extends Vue {
 
+    user: string = ''
     content: string = ''
 
     postTweet(): void {
       if (this.content === '') {
         return
       }
-      const user: string = 'hogehoge'
-      const content: string = this.content
-      this.$emit('postTweet', user, content)
+
+      this.$emit('postTweet', this.postUser(), this.content)
+
       this.content = ''
+    }
+
+    private postUser(): string {
+      if (this.user === '') {
+        return 'you'
+      }
+      return this.user
     }
 
   }
 </script>
 
 <style lang="scss" scoped>
+#user {
+  width: 300px;
+  padding: 5px;
+  border: 1px solid #EAEAEA;
+  border-radius: 10px;
+  background-color: #FFF;
+}
 #content {
   resize: none;
   height: 95px;
   width: 300px;
+  margin: 2px 0 0 0;
   padding: 5px;
   background-color: #FFF;
   border: 1px solid #EAEAEA;
+  border-radius: 10px;
   font-size: 13px;
 }
 #submit {
