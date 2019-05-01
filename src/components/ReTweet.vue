be4fbb67296d571a2b47be1da2c4313959498534
tocomi <tocomi@wish.ocn.ne.jp>
2019-05-01 16:31:00 +0900

    make retweet component

---
+++ src/components/ReTweet.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div.retweet
+    div.user-and-time
+      span.user {{ tweet.user }}
+      span.created_at {{ tweet.createdAt }}
+    span.content {{ tweet.content }}
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from "vue-property-decorator";
+  
+  @Component
+  export default class Tweet extends Vue {
+
+    @Prop() tweet!: any
+
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
